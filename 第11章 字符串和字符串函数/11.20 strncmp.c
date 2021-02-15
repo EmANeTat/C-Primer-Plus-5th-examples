7c3ad7102fc710e1f34b929e19dd5014e8d12bec
EmANeTat <juser2333@163.com>
2021-02-15 23:58:13 +0800

    cpp5_11

--- 第11章 字符串和字符串函数/11.20 strncmp.c
+++ 第11章 字符串和字符串函数/11.20 strncmp.c
@@ -1,1 +1,20 @@
+/* starsrch.c -- 使用strncmp.c ()函数 */
+#include <stdio.h>
+#include <string.h>
+#define LISTSIZE 5
+int main (void)
+{
+    char * list[LISTSIZE] = {"astronomy", "astounding", "astrophysics", "ostracize", "asterrism"};
+    int count = 0;
+    int i;
 
+    for (i = 0; i < LISTSIZE; i++)
+        if (strncasecmp (list[i], "astro", 5) == 0)
+        {
+            printf ("Found: %s\n", list[i]);
+            count++;
+        }
+    printf ("The list contained %d words beginning"" with astro.\n", count);
+
+    return 0;
+}
